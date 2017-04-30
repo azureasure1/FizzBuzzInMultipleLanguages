d97c9859d43a39009db5ea1596db12a2b01ddf25
Cody Griffin <griffincody38@yahoo.com>
2017-04-29 20:31:16 -0400

    Added the Python version of the FizzBuzz problem

---
+++ FizzBuzzInPython.py
@@ -0,0 +1,14 @@
+#Programmer: Cody Griffin
+#Date: 4/29/2017
+#Program: FizzBuzz in Python
+
+for num in range(1, 100):
+    if num % 15 == 0:
+        print (" FizzBuzz ");
+    elif num % 3 == 0:
+        print(" Fizz ");
+    elif num % 5 == 0:
+        print (" Buzz ");
+    else:
+        print (num);
+    
